2ef361de53c557b647983cb739f6cc299b0d0228
irom77 <irek.romaniuk@gmail.com>
2016-05-09 10:04:12 -0400

    adding build versioning

--- backup-notify/main.go
+++ backup-notify/main.go
@@ -9,7 +9,7 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 	"io/ioutil"
 	"log"
 	"flag"
@@ -19,12 +19,25 @@ import (
 	//"html/template"
 )
 
-//backup-notify
+var (
+	Version = "No Version Provided"
+	BuildTime = ""
+)
+//backup-notify -v to check version
 func main() {
+	//go run -ldflags "-X main.Version=1.0.1" github.com\irom77\go-public\backup-notify
+	/*go build -ldflags "-X main.BuildTime=`date -u +.%Y%m%d.%H%M%S` -X main.Version=1.0.1"
+	github.com\irom77\go-public\backup-notify*/
+	version := flag.Bool("v", false, "Prints current version")
+	flag.Parse()
+	if *version {
+		fmt.Printf("App Version: %s\nBuild Time : %s\n", Version, BuildTime)
+		os.Exit(0)
+	}
 	//Run program with path name i.e. -path="C:/" , default is current directory
 	backupdir := flag.String("path", ".", "Directory to look for files")
 	//Run program with file name i.e. -name="mdsbk.tgz" , default is mdsbk.tgz
-	namecontains := flag.String("name", "mdsbk.tgz", "File name or part of")
+	namecontains := flag.String("name", "mdsbk.tgz", "File name or part of it")
 	//Run program with file size i.e. -size="200000" , default is 9000000
 	backupsize := flag.Int64("size", 9000000, "Minimum file size")
 	m := gomail.NewMessage()
