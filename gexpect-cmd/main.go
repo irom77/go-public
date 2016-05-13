0af8979bc408bccc03114dd3482c730ee753e7bd
irom77 <irek.romaniuk@gmail.com>
2016-05-13 13:11:31 -0400

    New

--- gexpect-cmd/main.go
+++ gexpect-cmd/main.go
@@ -16,34 +16,34 @@ var (
 
 func init() { flag.Parse() }
 
-func main() { // OMIT
-exp, err := expect.Spawn( "ssh", *USER + '@' + *HOST )
-if err != nil { panic(err) }
-defer exp.Close()
+func main() {
+	// Spawn an expect process
+	ssh, err := expect.Spawn("ssh", *USER + '@' + *HOST)
+	ssh.SetTimeout(10 * time.Second)
+	const PROMPT = `#` // `(?m)[^$]*$`
 
-exp.SetTimeout(5 * time.Second) // HL
 
-exp.Expect(`[Pp]assword:`) // HL
-exp.Sendln(*PASS)
+	// Login
+	ssh.Expect(`[Pp]assword:`)
+	ssh.SendMasked(*PASS) // SendMasked hides from logging
+	ssh.Send("\n")
+	ssh.Expect(PROMPT) // Wait for prompt
 
-exp.Expect(`\$`) // HL
-exp.Sendln("ls -lh")
-exp.Expect("ls -lh") // Cut out remote-echo
+	// Run a command
+	ssh.SendLn("ls -lh")
+	match, err := ssh.Expect(PROMPT) // Wait for prompt
+	fmt.Println("ls -lh output:", match.Before)
 
-m, _ := exp.Expect(`(?m)^.*\$`) // HL
-fmt.Println("Directory Listing:", m.Before)
+	// Hit a timeout
+	ssh.SendLn("sleep 60") // This will cause a timeout
+	match, err = ssh.Expect(PROMPT) // This will timeout
+	if err == expect.ErrTimeout {
+		fmt.Println("Session timed out. Like we were expecting.\n")
+	}
 
-exp.Sendln("exit") // HL
-exp.ExpectEOF() // HL
-} // OMIT
-
-/*
-func problem() { // OMIT
-myPty := pty.Start(exec.Command("ssh", "localhost"))
-buffer := make([]byte, 4096)
-bytesRead, err := myPty.Read(buffer) // HL
-buffer = buffer[0:bytesRead] // Chop buffer slice to only contain what was read
-} // OMIT
-*/
+	// Wait for EOF
+	ssh.SendLn("exit")
+	ssh.ExpectEOF()
+}
 
 
