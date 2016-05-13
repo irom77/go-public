3f06fbfdde4e58b40b1c6dce5b29fafa1b1850a2
irom77 <irek.romaniuk@gmail.com>
2016-05-13 13:01:05 -0400

    New

--- gexpect-cmd/main.go
+++ gexpect-cmd/main.go
@@ -1,17 +1,30 @@
 package main
 
-import "github.com/jamesharr/expect"
+import (
+	"github.com/jamesharr/expect"
+	"time"
+	"fmt"
+	"flag"
+)
 
+var (
+	USER = flag.String("user", "manager", "ssh username") // or os.Getenv("USER") or os.Getenv("USERNAME")
+	HOST = flag.String("host", "127.0.0.1", "ssh server name")
+	PASS = flag.String("pass", "", "ssh password")
+	//CMD =  flag.String("cmd", "", "command to run")
+)
 
-func usage() { // OMIT
-exp, err := expect.Spawn( "ssh", "localhost" )
+func init() { flag.Parse() }
+
+func main() { // OMIT
+exp, err := expect.Spawn( "ssh", *USER + '@' + *HOST )
 if err != nil { panic(err) }
 defer exp.Close()
 
 exp.SetTimeout(5 * time.Second) // HL
 
 exp.Expect(`[Pp]assword:`) // HL
-exp.Sendln("terriblepassword")
+exp.Sendln(*PASS)
 
 exp.Expect(`\$`) // HL
 exp.Sendln("ls -lh")
@@ -24,9 +37,13 @@ exp.Sendln("exit") // HL
 exp.ExpectEOF() // HL
 } // OMIT
 
+/*
 func problem() { // OMIT
 myPty := pty.Start(exec.Command("ssh", "localhost"))
 buffer := make([]byte, 4096)
 bytesRead, err := myPty.Read(buffer) // HL
 buffer = buffer[0:bytesRead] // Chop buffer slice to only contain what was read
 } // OMIT
+*/
+
+
