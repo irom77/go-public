6ad2c548e9bec626b258eb657fcc9d936c299c88
irom77 <irek.romaniuk@gmail.com>
2016-08-13 08:38:42 -0400

    adding goex-gaia

--- goex-gaia/main.go
+++ goex-gaia/main.go
@@ -1,19 +1,51 @@
 package main
 
 import gexpect "github.com/ThomasRooney/gexpect"
-import "log"
+import (
+	"log"
+	"os"
+	"fmt"
+	"flag"
+)
+
+var (
+	USERHOST = flag.String("user", "manager@localhost", "ssh user@host")
+	PROMPT1 = flag.String("clish prompt", ">", "prompt1")
+	//PROMPT2 = flag.String("expert prompt ", "#", "prompt2")
+	PASS = flag.String("pass", "", "clish password")
+	//EXPERT = flag.String("expert", "", "expert password")
+	CMD =  flag.String("cmd", "fw stat", "command to run")
+	version = flag.Bool("v", false, "Prints current version")
+)
+var (
+	Version = "No Version Provided"
+	BuildTime = ""
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("Copyright 2016 @IrekRomaniuk. All rights reserved.\n")
+		fmt.Printf("Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if *version {
+		fmt.Printf("App Version: %s\nBuild Time : %s\n", Version, BuildTime)
+		os.Exit(0)
+	}
+}
 
 func main() {
 	log.Printf("Testing ssh... ")
 
-	child, err := gexpect.Spawn("ssh admin@10.199.107.1")
+	child, err := gexpect.Spawn("ssh " + USERHOST)
 	if err != nil {
 		panic(err)
 	}
 	child.Expect("password:")
-	child.SendLine("")
-	child.Expect("#")
-	child.SendLine("fw fetch")
+	child.SendLine(PASS)
+	child.Expect(PROMPT1)
+	child.SendLine(CMD)
 	child.Interact()
 	log.Printf("Success\n")
 	child.Close()
