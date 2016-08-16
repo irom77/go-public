8733ea12a52b217383d79229cbff45c6fae3abb0
irom77 <irek.romaniuk@gmail.com>
2016-08-16 14:24:14 -0400

    working on pingnet

--- pingnet/pingnet.go
+++ pingnet/pingnet.go
@@ -1,15 +1,41 @@
 package main
+//https://gist.github.com/kotakanbe/d3059af990252ba89a82
 import (
-	//"net"
-	//"os/exec"
-	"fmt"
-	//"os"
-	"time"
-	//"sync"
+	"net"
 	"os/exec"
+	"github.com/k0kubun/pp"
+	"fmt"
 )
 
-//var wg sync.WaitGroup
+type Pong struct {
+	Ip    string
+	Alive bool
+}
+
+func ping(pingChan <-chan string, pongChan chan<- Pong) {
+	for ip := range pingChan {
+		_, err := exec.Command("ping", "-c1", "-t1", ip).Output()
+		var alive bool
+		if err != nil {
+			alive = false
+		} else {
+			alive = true
+		}
+		pongChan <- Pong{Ip: ip, Alive: alive}
+	}
+}
+
+func receivePong(pongNum int, pongChan <-chan Pong, doneChan chan<- []Pong) {
+	var alives []Pong
+	for i := 0; i < pongNum; i++ {
+		pong := <-pongChan
+		//  fmt.Println("received:", pong)
+		if pong.Alive {
+			alives = append(alives, pong)
+		}
+	}
+	doneChan <- alives
+}
 
 func list1s() []string {
 	res := make([]string, 255) //256*64
@@ -22,46 +48,51 @@ func list1s() []string {
 	return res
 }
 
-func pinger(targets []string, ch chan<- string)  {
-	//defer wg.Done()
-	for _, ip := range targets {
-		_, err := exec.Command("ping", "-c", "1", "-w", "1", ip).Output()
-		if err == nil {
-			ch <- fmt.Sprintf(ip)
+func main() {
+	hosts := delete_empty(list1s())
+	concurrentMax := 200
+	pingChan := make(chan string, concurrentMax)
+	pongChan := make(chan Pong, len(hosts))
+	doneChan := make(chan []Pong)
+
+	fmt.Println(len(hosts), concurrentMax)
+	for i := 0; i < concurrentMax; i++ {
+		go ping(pingChan, pongChan)
+	}
+
+	go receivePong(len(hosts), pongChan, doneChan)
+
+	for _, ip := range hosts {
+		pingChan <- ip
+		//  fmt.Println("sent: " + ip)
+	}
+
+	alives := <-doneChan
+	pp.Println(len(alives))
+}
+
+func Hosts(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// remove network address and broadcast address
+	return ips[1 : len(ips)-1], nil
+}
+
+//  http://play.golang.org/p/m8TNTtygK0
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
 		}
 	}
-	//close(ch)
-}
-
-func pinger2(targets []string, ch chan<- string) {
-	for _, ip := range targets {
-		ch <- fmt.Sprintf(ip)
-	}
-}
-
-func printer (ch <-chan string) {
-	for {
-		msg := <-ch
-		fmt.Println(msg)
-	}
-}
-
-func printer2 (ch <-chan string) {
-	for msg := range ch {
-		fmt.Println(msg)
-	}
-}
-
-func main() {
-	start := time.Now()
-	targets := delete_empty(list1s())
-	fmt.Printf("%d->%s...%s\n",len(targets),targets[0], targets[len(targets)-1])
-	ch := make(chan string)
-	go pinger(targets, ch)
-	printer(ch)
-	//wg.Wait()
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-	//os.Args[1]
 }
 
 func delete_empty (s []string) []string {
@@ -73,5 +104,3 @@ func delete_empty (s []string) []string {
 	}
 	return r
 }
-
-
