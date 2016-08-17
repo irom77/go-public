5b3b766a8a068cfabf26dd1e4d45c45428a7b3ca
irom77 <irek.romaniuk@gmail.com>
2016-08-17 15:10:34 -0400

    working on pingnet

---
+++ fastping/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+	"github.com/tatsushid/go-fastping"
+	//"os"
+)
+
+
+func main() {
+	p := fastping.NewPinger()
+	/*ra, err := net.ResolveIPAddr("ip4:icmp", os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}*/
+	var hosts []string
+	hosts = delete_empty(list1s(193))
+
+	for _, ip := range hosts {
+		p.AddIP(ip)
+	}
+
+	//p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+	}
+	err := p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func list1s(limit2 int) []string {
+	res := make([]string, 256 * 64) //256*64
+	for x := 192; x < limit2; x++ {
+		//192-256
+		for y := 0; y < 256; y++ {
+			//0-256
+			res = append(res, fmt.Sprintf("10.%d.%d.1", x, y))
+			//fmt.Printf("10.%d.%d.1", x, y)
+		}
+	}
+	return res
+}
+
+func delete_empty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
+
+func tmp() {
+	pinger := fastping.NewPinger()
+
+	_, err := pinger.Network("udp")
+	// We shouldn't ever get an error but we're checking anyway
+	if err != nil {
+		panic("Error setting network type: " + err.Error())
+	}
+
+	addr, err := net.ResolveIPAddr("ip", "www.google.com")
+	if err != nil {
+		panic("Error resolving IP Address: " + err.Error())
+	}
+
+	pinger.AddIPAddr(addr)
+	pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("%s time=%v seconds\n", addr, rtt.Seconds())
+	}
+
+	if err = pinger.Run(); err != nil {
+		panic(err)
+	}
+}
