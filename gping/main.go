59dfdc741ff6394730f7f912dbd0ee61657b32ec
irom77 <irek.romaniuk@gmail.com>
2016-10-14 15:17:13 -0400

    initial

--- gping/main.go
+++ gping/main.go
@@ -104,9 +104,9 @@ func main() {
 func ping(ip string, wg *sync.WaitGroup, os string ) {
 	//_, err := exec.Command("ping", "-c 1", "-w 1", ip).Output()  //Linux
 	//result , err := exec.Command("ping", *PINGCOUNT, *PINGTIMEOUT, ip).Output()
-	_ , err := exec.Command("ping", os, *PINGCOUNT, "-w", *PINGTIMEOUT, ip).Output()
+	result , err := exec.Command("ping", os, *PINGCOUNT, "-w", *PINGTIMEOUT, ip).Output()
 	//_, err := exec.Command("ping", "-n 1", "-w 1", ip).Output()
-	//fmt.Printf("%s\n", result)
+	fmt.Printf("%s\n", result)
 	if err == nil {
 		count++
 		fmt.Printf("%d %s \n", count, ip)
